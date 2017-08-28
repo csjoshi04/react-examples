0f0871d65de9ed38cd2059c2a51a6d8cfbbff9af
Chandra Shekhar Joshi <cj.github@gmail.com>
2017-08-28 22:58:33 +0530

    throttling the search

--- app/youtubesearch/youtubesearch.index.js
+++ app/youtubesearch/youtubesearch.index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom'
 import React,{Component} from 'react';
 import YTSearch from 'youtube-api-search'
+import _ from 'lodash';
 import SearchBar from "./component/search-bar.component"
 import VideoList from "./component/video-list.component"
 import VideoDetail from "./component/video-detail.component"
@@ -27,9 +28,11 @@ class App extends Component{
     }
 
     render(){
+        const videoSearch = _.debounce((term)=>{this.videoSearch(term)},300)
+
         return(
             <div>
-                <SearchBar onSearchTermInputChange={(term)=>{this.videoSearch(term)}} />
+                <SearchBar onSearchTermInputChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={(selectedVideo)=>{this.setState({selectedVideo})}}
